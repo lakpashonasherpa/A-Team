c45ef3581b1c840563117d125d7ad3cbd10f2817
bhusan1 <43221027+bhusan1@users.noreply.github.com>
2020-08-07 21:45:07 -0500

    queryUtil working

--- src/main/java/com/vastika/Team_A_Account/util/QueryUtil.java
+++ src/main/java/com/vastika/Team_A_Account/util/QueryUtil.java
@@ -1,5 +1,13 @@
 package com.vastika.Team_A_Account.util;
 
 public class QueryUtil {
+	
+	public static final String INSERT_SQL_CUSTOMER = "insert into account_info(account_id, customer_name,customer_address,customer_mobileNum, customer_unique_id, customer_unique_id_number)values(?,?,?,?,?,?)";
+
+	public static final String DELETE_SQL_CUSTOMER = "delete from account_info where account_id =?";
+	
+	public static final String UPDATE_SQL_CUSTOMER = "update account_info set customer_name= ?, customer_address=?, customer_mobileNum =?, customer_unique_id =?, customer_unique_id_number=? where id=?";
+
+	public static final String GET_BY_ID_SQL = "select * from account_info where id = ?";
 
 }
